1b44164c42c163e732f9bd8361320fcf41dd3665
xzd9476 <xzd9476@163.com>
2019-06-04 20:19:18 +0800

    无操作

--- src/com/a/排序/D归并排序.java
+++ src/com/a/排序/D归并排序.java
@@ -12,42 +12,43 @@ public class D¹é²¢ÅÅĞò {
 		return A;
 	}
 
-	private static void sort(int[] A, int start, int end) {
-		if(end>start){
-			int middle=(start+end)/2;
-			sort(A,start,middle);
-			sort(A, middle+1, end);
-			merge(A,start,middle,end);
-		}
-	}
-
-	private static void merge(int[] A, int start, int middle, int end) {
-		int temp[]=new int[end-start+1];
-		int leftIndex=start;
-		int rightIndex=middle+1;
-		int tempIndex=0;
-		while(leftIndex<=middle && rightIndex <=end){
-			if(A[leftIndex] < A[rightIndex]){
-				temp[tempIndex++]=A[leftIndex++];
-			}else{
-				temp[tempIndex++]=A[rightIndex++];
-			}
-		}
-		
-		while(leftIndex<=middle){
-			temp[tempIndex++]=A[leftIndex++];
-		}
-		while(rightIndex<=end){
-			temp[tempIndex++]=A[rightIndex++];
-		}
-		
-		for (int i = 0; i <temp.length; i++) {
-			A[i+start]=temp[i];
-		}
-	}
+	private static void sort(int[] A,int start,int end){
+        if(end>start){
+            int middle=(start+end)/2;
+            sort(A,start,middle);
+            sort(A,middle+1,end);
+            merge(A,start,middle,end);
+        }
+    }
+    
+    private static void merge(int[] A,int start,int middle,int end){
+        int leftindex=start;
+        int rightindex=middle+1;
+        int tempindex=0;
+        int[] temp=new int[end-start+1];
+        while(leftindex<=middle && rightindex<=end){
+            if(A[leftindex]<A[rightindex]){
+                temp[tempindex++]=A[leftindex++];
+            }else{
+                temp[tempindex++]=A[rightindex++];
+            }
+        }
+        while(leftindex<=middle){
+            temp[tempindex++]=A[leftindex++];
+        }
+        while(rightindex<=end){
+            temp[tempindex++]=A[rightindex++];
+        }
+        for(int i=0;i<temp.length;i++){
+            A[i+start]=temp[i];
+        }
+    }
 	
 	public static void main(String[] args) {
 		int[] A={1,2,4,8,2,7,3,5};
+		for (int i : A) {
+			System.out.println(i);
+		}
 		mergeSort(A, 8);
 	}
 }
