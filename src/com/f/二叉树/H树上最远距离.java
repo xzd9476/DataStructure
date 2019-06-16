51842da659c8a9ac47a7508e7c86fac70bce3eea
Administrator <Administrator@USERCHI-EQQ7TQ1>
2019-06-16 18:02:51 +0800

    树上最远距离

--- src/com/f/二叉树/H树上最远距离.java
+++ src/com/f/二叉树/H树上最远距离.java
@@ -1,5 +1,30 @@
 package com.f.¶þ²æÊ÷;
 
 public class HÊ÷ÉÏ×îÔ¶¾àÀë {
+	public class TreeNode {
+		int val = 0;
+		TreeNode left = null;
+		TreeNode right = null;
+
+		public TreeNode(int val) {
+			this.val = val;
+		}
+	}
+
+	private int longest=0;
+	public int findLongest(TreeNode root) {
+		int depth=findDist(root);
+		return longest;
+	}
+	
+	private int findDist(TreeNode root) {
+		if(root == null)
+			return 0;
+		int left=findDist(root.left);
+		int right=findDist(root.right);
+		longest=Math.max((left+right)+1,longest);
+		return Math.max(left, right)+1;
+	}
+	
 
 }
